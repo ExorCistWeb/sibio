1d32eabc68d33bcb6ef1ec1ffc4e57a4be6937a4
MirMurMay <vladimirovich.aleksei.ru@gmail.com>
2024-10-22 22:12:09 +0700

    catalog_pages3

--- js/main.js
+++ js/main.js
@@ -192,3 +192,15 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.querySelectorAll('.form-group input').forEach(input => {
+    input.addEventListener('focus', () => {
+        input.nextElementSibling.classList.add('active');
+    });
+
+    input.addEventListener('blur', () => {
+        if (input.value === '') {
+            input.nextElementSibling.classList.remove('active');
+        }
+    });
+});
